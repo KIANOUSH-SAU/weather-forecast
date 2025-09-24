2138dffe356cf2470db3b829a3ca45e81a59e332
NOTNETKIA <seddighpour.kianoush@gmail.com>
2025-09-24 13:07:08 +0300

    Refactor project structure and update ESLint configuration; remove unused files and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      parserOptions: { ecmaFeatures: { jsx: true } },
       globals: globals.browser,
     },
+    plugins: { "react-refresh": reactRefresh },
+    rules: {
+      "react-refresh/only-export-components": "warn",
+    },
   },
-])
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+];
